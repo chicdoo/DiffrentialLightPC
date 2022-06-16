160c9304802df1d73a279bcf8752ab0706e3ea39
Eungdoo Kim <chicdoo@naver.com>
2022-06-17 03:03:18 +0900

    Make GUI

--- DiffrentialLightPC/DiffrentialLightPC/ControlPanel.xaml.cs
+++ DiffrentialLightPC/DiffrentialLightPC/ControlPanel.xaml.cs
@@ -27,6 +27,7 @@ namespace DiffrentialLightPC
         public ControlPanel()
         {
             InitializeComponent();
+            mSerialPort = new SerialPort();
         }
 
         private void ComboBox_OnDropDownOpened(object sender, EventArgs e)
@@ -49,13 +50,34 @@ namespace DiffrentialLightPC
                 mSerialPort.DataBits = 8;
                 mSerialPort.StopBits = StopBits.One;
                 mSerialPort.Parity = Parity.None;
-//                mSerialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_OnDataReceived);
+                mSerialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_OnDataReceived);
                 mSerialPort.Open();
             }
             else if (sender == CONNECTION_btnClose)
             {
                 Console.WriteLine("CONNECTION Close");
             }
+            else if ( sender == PWM_btnDuty )
+            {
+                String[] args = new String[2];
+                int ch = PWM_cbChannel.SelectedIndex + 1;
+
+                args[0] = ch.ToString();
+                args[1] = PWM_txtDuty.Text;
+
+                SendCommand("setPWMDuty", args);
+            }
+            else if ( sender == DAC_btnSet )
+            {
+                String[] args = new string[2];
+                int ch = DAC_cbChannel.SelectedIndex + 1;
+
+                args[0] = ch.ToString();
+                args[1] = DAC_txtData.Text;
+
+                SendCommand("setDAC", args);
+            }
+
             else if ( sender == LUMI_btnSet )
             {
 
@@ -92,8 +114,52 @@ namespace DiffrentialLightPC
 
 
             }
+            else if ( sender == LOG_btnClear )
+            {
+                LOG_txtInfo.Text = "";
+            }
         }
 
+        private void SerialPort_OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int readBytes;
+            char[] buffer = new char[SERIAL_PACKET_SIZE];
 
+            readBytes = mSerialPort.Read(buffer, 0, SERIAL_PACKET_SIZE);
+
+
+            Dispatcher.Invoke(new Action(delegate {
+                LOG_txtInfo.Text += new string(buffer, 0, readBytes);
+            }));
+        }
+
+        public void SendCommand(String cmd, String[] args)
+        {
+            char[] data = new char[SERIAL_PACKET_SIZE];
+            String command;
+
+            command = cmd;
+            command += " ";
+
+            for ( int i = 0; i < args.Length; i++ )
+            {
+                command += args[i];
+                command += " ";
+            }
+            
+            for ( int i = 0; i < SERIAL_PACKET_SIZE; i++ )
+            {
+                if ( i < command.Length )
+                {
+                    data[i] = command[i];
+                }
+                else
+                {
+                    data[i] = '\0';
+                }
+            }
+
+            mSerialPort.Write(data, 0, SERIAL_PACKET_SIZE);
+        }
     }
 }
